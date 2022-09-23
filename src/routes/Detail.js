cec093b209e6b33e8fb7ecb890cca832544875fe
zeein <zeein81@gmail.com>
2022-09-23 13:14:38 +0900

    니콜라스 리엑트

---
+++ src/routes/Detail.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+function Detail() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState([]);
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  console.log(movie);
+  return (
+    <div>
+      <img src={movie.large_cover_image} alt={movie.title} />
+      <h1>Detail</h1>
+      <h2>Title:{movie.title}</h2>
+    </div>
+  );
+}
+
+export default Detail;
